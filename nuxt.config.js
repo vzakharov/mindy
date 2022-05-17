6150489b7d9a6e3d191f55f0b410119514ddbf91
Vova <vzakharov@gmail.com>
2022-05-17 11:50:26 +0300

    fancy credentials html

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,5 +73,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+
+  server: {
+    port: process.env.PORT || 1250,
+  },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,7 @@
               <template v-else>
                 <strong
                   v-text="message.user.name"
-                  :class="{ 'text-primary': message.user.name === user.name }"
+                  :class="{ 'text-primary': user && message.user.name === user.name }"
                 />:&nbsp;
                 <span v-text="message.content" />
               </template>
@@ -38,7 +38,7 @@
                 <b-form-input
                   id="username"
                   :value="enteredName"
-                  @input="enteredName = $event.toLowerCase().replace(/[^a-z0-9-]+/g, '-')"
+                  @input="enteredName = $event.toLowerCase().replace(/[^a-z0-9-]+/g, '-').replace(/-+/g, '-')"
                   @change="createUser"
                   placeholder="Enter your name to start chatting"
                 />
@@ -49,7 +49,11 @@
               variant="danger"
               dismissible              
             >
-              Name unavailable. Please choose another.
+              This name is already taken. If that’s you, paste your token below:
+              <b-input
+                v-model="settings.mindy.token"
+                placeholder="mindy-..."
+              />
             </b-alert>
 
             <!-- Enter message -->
@@ -58,7 +62,7 @@
               class="mb-0"
             >
               <b-form-group
-                :label="user.name"
+                :label="user && user.name"
                 label-for="message"
               >
                 <b-form-input
@@ -87,6 +91,7 @@
   import axios from 'axios'
   import _ from 'lodash'
   import syncLocal from '~/plugins/syncLocal'
+  import QRCode from 'qrcode'
   
   const mindy = axios.create({
     baseURL: process.env.MINDY_API_URL,
@@ -107,27 +112,12 @@
         nameUnavailable: false,
         settings: {
           mindy: {
-            user: {
-              name: null,
-            },
             token: null,
           },
-        }
+        },
+        user: null
 
       }
-
-    },
-
-    computed: {
-
-      user: {
-        get() {
-          return this.settings.mindy.user
-        },
-        set(value) {
-          this.settings.mindy.user = value
-        }
-      },
 
     },
 
@@ -162,12 +152,66 @@
         try {
 
           let { data: token } = await mindy.post('/users', { name: enteredName })
-          this.user.name = enteredName
-          _.assign(this.settings.mindy, { token })
+          this.settings.mindy.token = token          
+
+          // Download an html with login info
+          // Link is /mindy?token=<token>
+          let { origin } = window.location
+          let url = `${origin}/mindy?token=${token}`
+          // let text = `Use the below link if you want to login to Mindy as “${enteredName}” from another device:\n\n${url}\n\nNOTE THAT LOST LOGIN INFORMATION CANNOT BE RECOVERED AS WE DO NOT STORE IT.`
+          let html = String.raw`
+            <!DOCTYPE html>
+            <html lang="en">
+              <head>
+                <meta charset="utf-8">
+                <title>Mindy credentials</title>
+                <!-- import bootstrap -->
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+                <!-- link to favicon -->
+                <link rel="icon" type="image/png" href="${origin}/favicon.png">
+                <!-- style, add margin to h2 -->
+                <style>
+                  h2 {
+                    margin-top: 1rem;
+                  }
+                </style>
+              </head>
+              <body>
+                <!-- center text -->
+                <div class="container mt-4 text-center">
+                  <h1>Mindy login credentials for ${enteredName}</h1>
+                  <!-- warning that login info is not stored -->
+                  <p class="text-danger">NOTE THAT LOST LOGIN INFORMATION CANNOT BE RECOVERED AS WE DO NOT STORE IT.</p>
+                  <h2>Login token</h2>
+                  <p>Here is your login token to log in as <code>${enteredName}</code>:</p>
+                  <pre>${token}</pre>
+                  <!-- Button to copy token -->
+                  <button id="copy-token" class="btn btn-outline-secondary"
+                    onclick="navigator.clipboard.writeText('${token}'); Object.assign(document.querySelector('#copy-token'), { disabled: true, innerText: 'Copied!' });"
+                  >
+                    Copy token
+                  </button>
+                  <h2>Login link</h2>
+                  <p>Use this link to login from another device:</p>
+                  <p><a href="${url}">${url}</a></p>
+                  <!-- QR code -->
+                  <h2>QR code</h2>
+                  <p>Scan this QR code to login from e.g. a mobile device:</p>
+                  <img src="${await QRCode.toDataURL(url)}" />
+                </div>
+              </body>
+            </html>
+          `
+          let filename = `Mindy credentials for ${enteredName}.html`
+          let blob = new Blob([html], { type: 'text/html' })
+          let link = document.createElement('a')
+          link.href = window.URL.createObjectURL(blob)
+          link.download = filename
+          link.click()
 
         } catch (error) {
 
-          if (error.response.status === 409) {
+          if (error.response?.status === 409) {
 
             this.nameUnavailable = true
 
@@ -199,6 +243,48 @@
           content: message,
         })
 
+      },
+
+    },
+
+    watch: {
+
+      '$route.query.token': {
+        immediate: true,
+        async handler(token) {
+          if ( !token ) return
+          this.settings.mindy.token = token
+        },
+      },
+
+      'settings.mindy.token': {
+        immediate: true,
+        async handler(token) {
+          if ( !token ) return
+          mindy.defaults.headers.Authorization = `Bearer ${token}`
+          try {
+
+            this.user = (
+              await mindy.get('/me')
+            ).data
+            this.nameUnavailable = false
+
+
+          } catch (error) {
+            this.settings.mindy.token = null
+            // If it's a 401, show a toast
+            if (error.response?.status === 401) {
+              this.$bvToast.toast(
+                'Invalid Mindy token. Please double-check your Mindy credentials. Unfortunately, lost tokens cannot be recovered as we do not store them.',
+                {
+                  title: 'Mindy',
+                  variant: 'danger',
+                  solid: true
+                }
+              )
+            }
+          }
+        },
       },
 
     }
