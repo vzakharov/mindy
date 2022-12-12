24d4c310e734c5e3af346d4d7f39886632740e11
Vova <vzakharov@gmail.com>
2022-12-12 22:21:31 +0100

    favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+      { rel: 'icon', type: 'image/x-icon', href: './favicon.png' }
     ]
   },
 
